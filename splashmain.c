f426fb20ee54d75500fc14e414ba627c58b5710f
AreebJ <68392559+WeebsGames@users.noreply.github.com>
2026-09-13 23:56:54 -0500

    temp loop in splashmain

--- splashmain.c
+++ splashmain.c
@@ -1,8 +1,18 @@
 #include"types.h"
 #include"x86.h"
 
-void splashmain() {
+// void splashmain() {
 
-    // REPLACE THIS INFINITE LOOP WITH YOUR CODE
-    while(1);
+//     // REPLACE THIS INFINITE LOOP WITH YOUR CODE
+    
+//     while(1);
+// }
+
+void
+splashmain(void)
+{
+    volatile unsigned short *video = (volatile unsigned short *)0xB8000;
+    video[0] = 0x0F48;  // H
+    while (1)
+        ;
 }
