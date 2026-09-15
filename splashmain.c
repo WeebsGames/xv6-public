4e17605b9deff907e090c22028869c8508ec4e6d
AreebJ <68392559+WeebsGames@users.noreply.github.com>
2026-09-14 23:47:09 -0500

    attempted to make a smiley face

--- splashmain.c
+++ splashmain.c
@@ -5,8 +5,12 @@ void
 splashmain(void)
 {
     volatile unsigned char *video = (volatile unsigned char *)0xA0000;
-    for(int i = 0; i < 320; i+=2){
-        video[i + (320 * i)] = 0x0f;  // Set each pixel to a default color (black background, white foreground)
+    for(int i = 0; i < 20; i+=2){
+        video[30 + 320*10 + (320 * i)] = 0x0f;
+        video[60 + 320*10 + (320 * i)] = 0x0f;
+    }
+    for(int i = 0; i < 30; i++){
+        video[50*320 + 30 + i + (-320*((i*i)/30) + 30)] = 0x0f;
     }
     while (1)
         ;
